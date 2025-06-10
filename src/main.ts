b91a979be0ebf17a1857a6d4b73639609c235b25
guruprakash-c <c.guruprakash@yahoo.com>
2025-06-10 16:29:06 +0530

    ++

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,16 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
-import { SignalsComponent } from './components/signals/signals.component';
-import { HeaderComponent } from "./layouts/header/header.component";
-import { FooterComponent } from "./layouts/footer/footer.component";
-import { LoaderComponent } from "./layouts/loader/loader.component";
-import { BuiltInPipesComponent } from './components/built-in-pipes/built-in-pipes.component';
-import { TemplateDrivenFormComponent } from './components/template-driven-form/template-driven-form.component';
-import { ReactiveFormsComponent } from './components/reactive-forms/reactive-forms.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsComponent],
+  imports: [RouterOutlet],
   template: `
     <!--app-header/>
     <main>
     <app-loader/>
     </main>  
-    <app-footer/>
-    <app-built-in-pipes/-->
-    <app-reactive-forms />
+    <app-footer/>-->
     <router-outlet/>
   `,
 })
